fdb0d2dbdecd05041ec998707536cd8568331d6f
liulx <liulx@knownsec.com>
2015-01-04 23:28:18 +0800

    Extracted position from Appearance table of PIT players

--- defensive_spectrum.R
+++ defensive_spectrum.R
@@ -1,5 +1,3 @@
-require("magrittr")
-require("dplyr")
 packages <- c("magrittr", "dplyr")
 new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
@@ -13,11 +11,31 @@ pit.plyr.id <- read.csv("resources/lahman/Appearances.csv") %>%
     filter(teamID=="PIT") %>%
     distinct(playerID) %>%
     .$playerID
-
 pit.plyr <- master %>% filter(playerID %in% pit.plyr.id) %>%
     arrange(desc(debut))
 
-# TODO
-# Provide a function to determin the position that a player appeared in a season
+# Get the position that a player appeared in a season
+Get.Position <- function(x)
+{
+    # Select columns correspond to defensive positions
+    x[,10:17] %>% apply(1, function(i){
+        names(i)[which.max(i)]
+    })
+}
+
+# Get fielders (not pitcher, and number of games played larger than 0)
+pit.fielder <- read.csv("resources/lahman/Appearances.csv") %>%
+    filter(teamID=="PIT", G_p==0, G_all!=0)
+
+# Add position to pit fielders
+pos <- Get.Position(pit.fielder)
+pit.fielder <- cbind(pit.fielder,pos)
+
+# Extract year, team, player and position only
+pit.fielder <- pit.fielder %>%
+    mutate(position=toupper(substr(pos,3,length(pos)))) %>%
+    select(playerID, teamID, position, yearID) %>%
+    arrange(playerID, yearID)
+
 
 
